5913dd24a49f3dd9f5429bafaae25bb0b8ae5673
longjincen.try <longjincen.try@bytedance.com>
2022-07-05 23:07:01 +0800

    feat: 阅读 vue 入口模块

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -13,10 +13,12 @@ if (__DEV__) {
 
 const compileCache: Record<string, RenderFunction> = Object.create(null)
 
+// 对 compile 方法包装一层，将 template 编译为 render function
 function compileToFunction(
   template: string | HTMLElement,
   options?: CompilerOptions
 ): RenderFunction {
+  // 如果是一个 dom，那么取其 innerHTML 作为 template
   if (!isString(template)) {
     if (template.nodeType) {
       template = template.innerHTML
@@ -25,13 +27,13 @@ function compileToFunction(
       return NOOP
     }
   }
-
+  // 将 template 作为缓存的 key
   const key = template
   const cached = compileCache[key]
   if (cached) {
     return cached
   }
-
+  // 如果 template 传递的是一个选择器，那么获取对应的 el，并将其 innerHTML 作为 template
   if (template[0] === '#') {
     const el = document.querySelector(template)
     if (__DEV__ && !el) {
@@ -55,7 +57,7 @@ function compileToFunction(
       options
     )
   )
-
+  // dev 环境下，onError 处理函数
   function onError(err: CompilerError, asWarning = false) {
     const message = asWarning
       ? err.message
@@ -74,16 +76,19 @@ function compileToFunction(
   // with keys that cannot be mangled, and can be quite heavy size-wise.
   // In the global build we know `Vue` is available globally so we can avoid
   // the wildcard object.
+  // 创建 render 函数
   const render = (
     __GLOBAL__ ? new Function(code)() : new Function('Vue', code)(runtimeDom)
   ) as RenderFunction
 
   // mark the function as runtime compiled
+  // 标记该 rende 函数是 runtime 的时候编译出来的
   ;(render as InternalRenderFunction)._rc = true
-
+  // 将编译 template 对应的 render 缓存起来
   return (compileCache[key] = render)
 }
 
+// 注册 runtime 阶段使用的 compiler，将 @vue/runtime-core 模块内的 compiler 变量替换为当前的 compileToFunction
 registerRuntimeCompiler(compileToFunction)
 
 export { compileToFunction as compile }
